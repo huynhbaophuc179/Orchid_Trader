7fb14df317fe25fc1d4a67a49a9e401cbd2e6b28
Nguyễn Phước Tùng <npt@netcompany.com>
2023-07-19 22:22:52 +0700

    add side bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import 'react-native-gesture-handler';
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./src/screens/Login";
@@ -16,137 +15,143 @@ import Icon from "react-native-vector-icons/Ionicons";
 import Cart from "./src/screens/Cart";
 import Order from "./src/screens/Order";
 import Profile from "./src/screens/Profile";
+import {createDrawerNavigator, DrawerItem, DrawerItemList} from "@react-navigation/drawer";
+import { Button } from "react-native-paper";
+import Constants from "expo-constants";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-const InnerStack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function HomeLayout() {
-  return (
-    <InnerStack.Navigator>
-      <InnerStack.Screen
-        name="Orchid Home"
-        component={List}
-      ></InnerStack.Screen>
-      <InnerStack.Screen
-        // initialParams={ product: Product }}
-
-        name="Detail"
-        component={Detail}
-        options={{}}
-      ></InnerStack.Screen>
-      <InnerStack.Screen
-        // initialParams={ product: Product }}
-
-        name="OrderScreen"
-        component={Order}
-        options={{}}
-      ></InnerStack.Screen>
-      <InnerStack.Screen
-        // initialParams={ product: Product }}
-
-        name="Cart"
-        component={Cart}
-        options={{}}
-      ></InnerStack.Screen>
-    </InnerStack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Orchid Home"
+                component={List}
+            />
+            <Stack.Screen
+                name="Detail"
+                component={Detail}
+            />
+        </Stack.Navigator>
+    );
 }
+
 function InsideLayout() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Orchid Home"
-        component={HomeLayout}
-        options={{
-          headerShown: false,
-          headerTintColor: "black",
-          tabBarLabelStyle: {
-            color: "black",
-            fontWeight: "bold",
-          },
-          tabBarIcon: ({ focused }) => (
-            <Icon name={"ios-home"} size={25} color="black" />
-          ),
-        }}
-      ></Tab.Screen>
-      <Tab.Screen
-        name="Cart"
-        component={Cart}
-        options={{
-          headerShown: true,
-          headerTintColor: "black",
-          tabBarLabelStyle: {
-            color: "black",
-            fontWeight: "bold",
-          },
-          tabBarIcon: ({ focused }) => (
-            <Icon name={"cart"} size={25} color="black" />
-          ),
-        }}
-      ></Tab.Screen>
-      <Tab.Screen
-        name="Order"
-        component={Order}
-        options={{
-          headerShown: true,
-          headerTintColor: "black",
-          tabBarLabelStyle: {
-            color: "black",
-            fontWeight: "bold",
-          },
-          tabBarIcon: ({ focused }) => (
-            <Icon name={"list"} size={25} color="black" />
-          ),
-        }}
-      ></Tab.Screen>
-        <Tab.Screen
-            name="Profile"
-            component={Profile}
-            options={{
-                headerShown: true,
-                headerTintColor: "black",
-                tabBarLabelStyle: {
-                    color: "black",
-                    fontWeight: "bold",
-                },
-                tabBarIcon: ({ focused }) => (
-                    <Icon name={"list"} size={25} color="black" />
-                ),
-            }}
-        ></Tab.Screen>
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Orchid Home"
+                component={HomeLayout}
+                options={{
+                    headerShown: false,
+                    tabBarLabelStyle: {
+                        color: "black",
+                        fontWeight: "bold",
+                    },
+                    tabBarIcon: ({ focused }) => (
+                        <Icon name={"ios-home"} size={25} color="black" />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Cart"
+                component={Cart}
+                options={{
+                    tabBarLabelStyle: {
+                        color: "black",
+                        fontWeight: "bold",
+                    },
+                    tabBarIcon: ({ focused }) => (
+                        <Icon name={"cart"} size={25} color="black" />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Order"
+                component={Order}
+                options={{
+                    tabBarLabelStyle: {
+                        color: "black",
+                        fontWeight: "bold",
+                    },
+                    tabBarIcon: ({ focused }) => (
+                        <Icon name={"list"} size={25} color="black" />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarLabelStyle: {
+                        color: "black",
+                        fontWeight: "bold",
+                    },
+                    tabBarIcon: ({ focused }) => (
+                        <Icon name={"person"} size={25} color="black" />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
 }
 
 export default function App() {
-  const [user, setUser] = useState<User | null>(null);
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, (user) => {
-      setUser(user);
-      console.log("user", user);
-    });
+    const [user, setUser] = useState<User | null>(null);
 
-    return () => {};
-  }, []);
+    useEffect(() => {
+        onAuthStateChanged(FirebaseAuth, (user) => {
+            setUser(user);
+            console.log("user", user);
+        });
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        {user ? (
-          <Stack.Screen
-            name="Home"
-            component={InsideLayout}
-            options={{ headerShown: false }}
-          />
-        ) : (
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{ headerShown: true }}
-          />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+        return () => {};
+    }, []);
+
+    const handleSignOut = async () => {
+        try {
+            await FirebaseAuth.signOut();
+            console.log("User signed out successfully");
+        } catch (error) {
+            console.error("Error signing out:", error);
+        }
+    };
+
+    const DrawerContent = (props: any) => (
+        <View style={styles.drawerContainer}>
+            <DrawerItemList {...props} />
+            <DrawerItem label={"Signout"} onPress={handleSignOut} />
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                {user ? (
+                    <Drawer.Navigator drawerContent={DrawerContent}>
+                        <Drawer.Screen name="Home" component={InsideLayout} />
+                        <Drawer.Screen name="Profile" component={Profile} />
+                    </Drawer.Navigator>
+                ) : (
+                    <Stack.Navigator initialRouteName="Login">
+                        <Stack.Screen name="Login" component={Login} />
+                    </Stack.Navigator>
+                )}
+            </NavigationContainer>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+    },
+    drawerContainer: {
+        flex: 1,
+        paddingTop: Constants.statusBarHeight,
+    },
+});
